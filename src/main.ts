8b846b2eb7dac36e3886b574bba232d5191492fe
Silin7 <silin7@foxmail.com>
2026-03-19 22:50:57 +0800

    main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 import "element-plus/dist/index.css";
-import "../public/css/index.css";
+
+// 全局样式
+// import "../public/css/index.css";
 
 import App from "./App.vue";
 
